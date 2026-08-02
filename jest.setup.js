ec0ec93f26981ae636431b4caa5ec7349e666370
jieun <jisilver.kim@gmail.com>
2026-08-02 23:47:57 +0900

    feat: 테스트 코드 라이브러리 추가

---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+import '@testing-library/react-native/extend-expect';
+
+// 자주 쓰이는 라이브러리의 전역 모킹(Mocking)이 필요하다면 아래에 추가합니다.
+// 예시: react-native-reanimated, @react-native-async-storage/async-storage 등
