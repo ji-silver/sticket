84ad24ffaacaa9da7d8cfd4f1fc1652856272749
jieun <jisilver.kim@gmail.com>
2026-08-13 15:41:56 +0900

    fix: 티켓북 삭제 후 캘린더 업데이트

--- src/features/ticket-book/api/useDeleteTicketBook.ts
+++ src/features/ticket-book/api/useDeleteTicketBook.ts
@@ -1,13 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { TICKETS_QUERY_KEY } from '../../ticket/api/useGetTickets';
 import { deleteTicketBook } from '../ticketBook.service';
+import { TICKET_BOOKS_QUERY_KEY } from './useGetTicketBooks';
 
 export function useDeleteTicketBook() {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (ticketBookId: string) => deleteTicketBook(ticketBookId),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['ticketBooks'] });
-      queryClient.invalidateQueries({ queryKey: ['bucketList'] });
-    },
+    onSuccess: () =>
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: TICKET_BOOKS_QUERY_KEY }),
+        queryClient.invalidateQueries({ queryKey: ['bucketList'] }),
+        queryClient.invalidateQueries({ queryKey: TICKETS_QUERY_KEY }),
+        queryClient.invalidateQueries({ queryKey: ['attendanceSummary'] }),
+      ]),
   });
 }
